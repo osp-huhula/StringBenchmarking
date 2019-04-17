f5416bce40dcb4eaaada341dd523d54638ba9cc8
João Brasileiro <jcbrasileiro@hotmail.com>
2019-04-16 22:14:14 -0300

    add live converter for timeout.

---
+++ src/main/java/stringbenchmarking/result/converter/line/TimeoutLineConverter.java
@@ -0,0 +1,23 @@
+package stringbenchmarking.result.converter.line;
+
+import java.util.regex.Matcher;
+
+import stringbenchmarking.commons.CommonsMatcher;
+import stringbenchmarking.result.beans.WarmUp;
+
+public final class TimeoutLineConverter
+	implements
+	JMHResultLineConverter<Integer> {
+
+	@Override
+	public Integer converter(
+		String content) {
+		String regex = regex();
+		Matcher matcher = CommonsMatcher.matcher(regex, content);
+		return Integer.valueOf(matcher.group(1));
+	}
+
+	private String regex() {
+		return "# Timeout: ([0-9]+) min per iteration";
+	}
+}
