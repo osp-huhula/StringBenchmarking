a9dffd58272f8cb4d1400708e877e6b50cecbf66
João Brasileiro <jcbrasileiro@hotmail.com>
2019-04-24 21:06:02 -0300

    update parametrized test and unit-test

--- src/test/java/stringbenchmarking/result/BenchmarkResultConverterDefaultTest.java
+++ src/test/java/stringbenchmarking/result/BenchmarkResultConverterDefaultTest.java
@@ -1,18 +1,19 @@
 package stringbenchmarking.result;
 
-import static org.junit.Assert.fail;
-
 import java.io.File;
 import java.util.ArrayList;
 
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 import stringbenchmarking.commons.FileReader;
+import stringbenchmarking.commons.exception.UnexpectedEOF;
+import stringbenchmarking.commons.zuz.ZuzObjects;
 import stringbenchmarking.result.beans.JMHResult;
-import stringbenchmarking.result.beans.JMHResultImp;
 import stringbenchmarking.result.converter.BenchmarkResultConverterDefault;
 
 
@@ -24,41 +25,45 @@ public class BenchmarkResultConverterDefaultTest {
 	@Parameters(name= "{index}: converting file[{0}]={1}")
 	public static Iterable<Object[]> data() {
 		ArrayList<Object[]> data = new ArrayList<Object[]>();
-//		data.add(new Object[] {new File("benchmark_mode-Throughput-single.log"), new JMHResultImp()});
-//		data.add(new Object[] {new File("benchmark_mode-AverageTime-single.log"), new JMHResultImp()});
-//		data.add(new Object[] {new File("benchmark_mode-SampleTime-single.log"), new JMHResultImp()});
-//		data.add(new Object[] {new File("benchmark_mode-SingleShotTime-single.log"), new JMHResultImp()});
-//		data.add(new Object[] {new File("benchmark_mode-All-single.log"), new JMHResultImp()});
-//		data.add(new Object[] {new File("benchmark_mode-Throughput-multiple.log"), new JMHResultImp()});
-		data.add(new Object[] {new File("benchmark_mode-AverageTime-multiple.log"), new JMHResultImp()});
-//		data.add(new Object[] {new File("benchmark_mode-SampleTime-multiple.log"), new JMHResultImp()});
-//		data.add(new Object[] {new File("benchmark_mode-SingleShotTime-multiple.log"), new JMHResultImp()});
-//		data.add(new Object[] {new File("benchmark_mode-All-multiple.log"), new JMHResultImp()});
-//		data.add(new Object[] {new File("benchmark_mode-multiple-different.log"), new JMHResultImp()});
+//		data.add(new Object[] {new File("benchmark_mode-Throughput-single"), new JMHResultImp()});
+//		data.add(new Object[] {new File("benchmark_mode-AverageTime-single"), new JMHResultImp()});
+//		data.add(new Object[] {new File("benchmark_mode-SampleTime-single"), new JMHResultImp()});
+//		data.add(new Object[] {new File("benchmark_mode-SingleShotTime-single"), new JMHResultImp()});
+//		data.add(new Object[] {new File("benchmark_mode-All-single"), new JMHResultImp()});
+//		data.add(new Object[] {new File("benchmark_mode-Throughput-multiple"), new JMHResultImp()});
+		data.add(new Object[] {new File("benchmark_mode-AverageTime-multiple")});
+//		data.add(new Object[] {new File("benchmark_mode-SampleTime-multiple"), new JMHResultImp()});
+//		data.add(new Object[] {new File("benchmark_mode-SingleShotTime-multiple"), new JMHResultImp()});
+//		data.add(new Object[] {new File("benchmark_mode-All-multiple"), new JMHResultImp()});
+//		data.add(new Object[] {new File("benchmark_mode-multiple-different"), new JMHResultImp()});
 		return data;
 	}
 	
 	private final FileReader reader = new FileReader();
 	private final BenchmarkResultConverterDefault converter = new BenchmarkResultConverterDefault();
 
-    private File file;
-    private JMHResult expected;
-    
+	private File file;
+
 	public BenchmarkResultConverterDefaultTest(
-		File file,
-		JMHResult expected) {
+		File file) {
 		super();
 		this.file = file;
-		this.expected = expected;
 	}
 
 	@Test
-	public void test() {
-		String content = reader.readFile("result/".concat(file.getName()));
-		System.out.println(content);
+	public void converter()
+		throws UnexpectedEOF {
+		String content = reader.readFile("result/" + file.getName() + ".log");
+		String expectedContent = reader.readFile("expected/" + file.getName());
 		JMHResult result = converter.converter(content);
-		System.err.println(result.toString());
-//		Assert.assertEquals(expected, result);
-		fail("Not yet implemented");
+		String actual = toString(result);
+		Assert.assertEquals(expectedContent, actual);
 	}
+
+	private String toString(
+		JMHResult result) {
+		ZuzObjects.changeDefaultStyle(ToStringStyle.SHORT_PREFIX_STYLE);
+		return ZuzObjects.reflectionToString(result);
+	}
+	
 }
