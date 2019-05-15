9f1d8928f3edcc5e1df5a4e8063d7b4dee7070b3
João Brasileiro <jcbrasileiro@hotmail.com>
2019-05-15 20:21:01 -0300

    add support for Throughput (many)

--- src/test/java/stringbenchmarking/result/converter/line/average/ResultAverageLineConverterTest.java
+++ src/test/java/stringbenchmarking/result/converter/line/average/ResultAverageLineConverterTest.java
@@ -11,11 +11,20 @@ public class ResultAverageLineConverterTest {
 	private ResultAverageLineConverter converter = new ResultAverageLineConverter();
 
 	@Test
-	public void converter() {
+	public void converter01() {
 		ResultForkAverageInfo result = converter.converter("  3,291 �(99.9%) 3,024 ns/op [Average]");
 		Assert.assertEquals("3,291", result.getScore());
 		Assert.assertEquals("99.9", result.getPercent());
 		Assert.assertEquals("3,024", result.getError());
 		Assert.assertEquals("ns/op", result.getUnit());
+	}
+	
+	@Test
+	public void converter02() {
+		ResultForkAverageInfo result = converter.converter("  0,135 �(99.9%) 0,174 ops/ns [Average]");
+		Assert.assertEquals("0,135", result.getScore());
+		Assert.assertEquals("99.9", result.getPercent());
+		Assert.assertEquals("0,174", result.getError());
+		Assert.assertEquals("ops/ns", result.getUnit());
 	}
 }
